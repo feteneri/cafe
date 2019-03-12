317310a056314face14209bcb02440773d1ff395
feteneri <fe.teneri@gmail.com>
2019-03-12 14:17:55 +0500

    fixed highlight function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,8 @@
 var elements = document.getElementsByClassName('complex-types__rectangle');
 function highlight(element) {
+  for (let i = 0; i < elements.length; i++)  {
+    elements[i].classList.remove('complex-types__rectangle_selected');
+  }
   element.classList.add('complex-types__rectangle_selected');
 }
 for (let i = 0; i < elements.length; i++) {
